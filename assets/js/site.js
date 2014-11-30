4765d39630944ee9af6d8385f8dcd52b73c04048
Nick <nick.j.ratcliffe@gmail.com>
2014-11-30 19:56:35 +0000

    first commit

---
+++ assets/js/site.js
@@ -0,0 +1,6 @@
+// @codekit-prepend "libs/fastclick.js"
+// @codekit-prepend "libs/jquery-2.1.1.js"
+
+// @codekit-prepend "plugins/jquery-simpleselect.js"
+
+// @codekit-prepend "core/global.js"
---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+		<footer class="container full-width">
+
+			<p><a href="#">&copy; Engage Interactive <?=date('Y')?></a></p>
+
+		</footer>
+
+	</div>
+
+	<!-- JS -->
+	<script src="/assets/js/min/site.min.js"></script>
+
+</body>
+</html>
